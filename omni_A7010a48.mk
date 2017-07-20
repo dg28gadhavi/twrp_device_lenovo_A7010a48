a17a424fc0e3f4a0cf5842cb670766f6886caa66
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-07-20 17:34:10 +0530

    Added Filesystem management tools

--- omni_A7010a48.mk
+++ omni_A7010a48.mk
@@ -5,15 +5,20 @@ PRODUCT_COPY_FILES := \
     bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 # Filesystem management tools
-	PRODUCT_PACKAGES += \
-	    e2fsck \
-	    fibmap.f2fs \
-	    fsck.f2fs \
-	    mkfs.f2fs \
-	    make_ext4fs \
-	    resize2fs \
-	    setup_fs \
-	    mount.exfat \
+PRODUCT_PACKAGES += \
+    e2fsck \
+    fibmap.f2fs \
+    fsck.f2fs \
+    mkfs.f2fs \
+    make_ext4fs \
+    resize2fs \
+    setup_fs \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat \
+    fsck.ntfs \
+    mkfs.ntfs \
+    mount.ntfs
 
 # TWRP
 PRODUCT_COPY_FILES += \
