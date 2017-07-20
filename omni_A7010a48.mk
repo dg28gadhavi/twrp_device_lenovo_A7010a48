a30ed259d67986129a001270112cc052f0a74897
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-07-20 17:31:43 +0530

    Added Filesystem management tools

--- omni_A7010a48.mk
+++ omni_A7010a48.mk
@@ -4,6 +4,17 @@ PRODUCT_COPY_FILES := \
     device/lenovo/A7010a48/prebuilt/Image.gz-dtb:kernel \
     bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
+# Filesystem management tools
+	PRODUCT_PACKAGES += \
+	    e2fsck \
+	    fibmap.f2fs \
+	    fsck.f2fs \
+	    mkfs.f2fs \
+	    make_ext4fs \
+	    resize2fs \
+	    setup_fs \
+	    mount.exfat \
+
 # TWRP
 PRODUCT_COPY_FILES += \
           $(LOCAL_PATH)/recovery/etc/twrp.fstab:recovery/root/etc/twrp.fstab
